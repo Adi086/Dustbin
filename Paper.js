0a6ac4b7b4e6196f4c1f7a32b7d5380ef1b6c23b
Adi086 <77397630+Adi086@users.noreply.github.com>
2021-04-19 10:17:41 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,28 @@
+class Paper {
+    constructor(x, y, radius) {
+    var c = {
+        'restitution':0.8,
+        'friction':1.5,
+        'density':1.0
+    }
+    this.radius= radius;
+    this.body = Bodies.circle(x, y,radius, c);
+    
+
+    World.add(world, this.body);
+  }
+  display(){
+    var pos =this.body.position;
+    ellipseMode(CENTER)
+    fill("red")
+    ellipse(pos.x, pos.y, this.radius);
+    
+  }
+}
+
+/*class Paper extends BaseClass{
+  constructor(x,y,radius){
+    super(x,y,20)
+    this.image=loadImage("sprites/paper.png")
+  }
+}*/
